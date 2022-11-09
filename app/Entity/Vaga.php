d44401eee094cefb388f91bc40c430a64f778c2f
Lucas Praxedes <luk_288@hotmail.com>
2022-11-09 10:29:55 -0300

    first commit

---
+++ app/Entity/Vaga.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Entity;
+
+use App\Db\Database;
+
+class Vaga 
+{
+    public $id;
+    public $titulo;
+    public $descricao;
+    public $ativo;
+    public $data;
+
+    public function cadastrar()
+    {
+        $this->data = date('Y-m-d H:i:s');
+
+        $obDatabase = new Database('vagas');
+        
+    }
+}
---
+++ cadastrar.php
@@ -0,0 +1,18 @@
+<?php 
+require __DIR__.'/vendor/autoload.php';
+
+use \App\Entity\Vaga;
+
+//echo "<pre>"; print_r($_POST); echo "</pre>"; exit;
+//VALIDAÇÃO DO POST
+if(isset($_POST['titulo'],$_POST['descricao'],$_POST['ativo'])){
+    $obVaga = new Vaga;
+    $obVaga->titulo = $_POST['titulo'];
+    $obVaga->descricao = $_POST['descricao'];
+    $obVaga->ativo = $_POST['ativo'];
+    $obVaga->cadastrar();
+}
+
+include __DIR__.'/includes/header.php';
+include __DIR__.'/includes/formulario.php';
+include __DIR__.'/includes/footer.php';
